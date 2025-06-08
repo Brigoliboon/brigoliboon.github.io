6214eeecb43f46fbd7315f891b05445a50d97a01
“Boon <brigoliboonjefferson@gmail.com>
2025-06-08 14:24:27 +0800

    modify config

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,9 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+   output: 'export',
+  basePath: '/your-repo-name', // Replace with your repo name
+  trailingSlash: true,    
 };
 
 export default nextConfig;
